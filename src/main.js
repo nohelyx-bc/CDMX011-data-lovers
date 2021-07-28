1236c3d877b0f9bfb5c3e08c827fa0da9d05837e
Alba <alba.lp95@gmail.com>
2021-07-28 11:28:24 -0500

    Visualización de personajes y algunos estilos

--- src/main.js
+++ src/main.js
@@ -2,19 +2,35 @@
 // import data from './data/lol/lol.js';
 
 import ghibli from './data/ghibli/ghibli.js';
-console.log(ghibli);
+
 let peliculas = document.getElementById("peliculas")
+let personajes = document.getElementById("personajes");
+
+
 const allFilms = ghibli.films.map((film) => {
     
-    return `<img  src="${film.poster}"> <h2>${film.title}</h2> `
+    return `<div class="column">
+                <h2 class="title">${film.title}</h2> 
+                <img class="row" src="${film.poster}">  
+                <p class="row">${film.description}</p>
+                <p class="row">· Director: ${film.director}</p>     
+                <p>· Productor: ${film.producer}</p> 
+                <p>· Año de estreno: ${film.release_date}</p>
+                <p>· Rotten Tomatoes: ${film.rt_score}</p>
+                <p>· Personajes: ${film.people.map(personaje => {return `${personaje.name}`
+                }
+                    )}</p>
+            </div>` 
 }) 
 console.log(allFilms);
 peliculas.innerHTML = allFilms;
-/*function showMovie (films) { 
-    
-    return `Probando ${films.id}, Probando ${films.description}`;
-       
-}*/
+
+
+/* const pers = ghibli.films.map(film) =>{
+    return `${film.people.map(personaje => {return `${personaje.name}`
+}
+personajes.innerHTML =pers;*/
+
 console.log(ghibli.films);
 //showMovie(films);
 //console.log(showMovie);
