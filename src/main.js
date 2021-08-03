7775f15ae076be6c9d353a4d2b5eb4cfc5a62f4f
Alba <alba.lp95@gmail.com>
2021-08-03 18:35:16 -0500

    Nav corregido

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@
 // import data from './data/lol/lol.js';
 import { allFilms } from './data.js';
 import ghibli from './data/ghibli/ghibli.js';
-
-let allInfo = new allFilms;
+console.log(allFilms);
+//let allInfo = new allFilms;
 let movies = document.getElementById("movies")
-let rt = document.getElementById("rt");
-let alphabet = document.getElementById("alphabet").value;
+//let rt = document.getElementById("rt");
+//let alphabet = document.getElementById("alphabet").value;
 //let personajes = document.getElementById("personajes");
+
 
 ghibli.films.forEach((film) => {
             movies.innerHTML +=
@@ -16,24 +17,46 @@ ghibli.films.forEach((film) => {
 <div class="column">
 
 <h2>${film.title}</h2> 
-<img src="${film.poster}">  
-<p>${film.description}</p>
+<img src="${film.poster}" class="poster">  
+<p class="description">${film.description}</p>
 <!-- 
 <p>· Personajes: ${film.people.map(personaje => {return `${personaje.name}`
 }
 )}</p> -->
 </div>
 <div> 
-<ul>
-<li type="circle">Director: ${film.director}</li>    
-<li type="circle">Productor: ${film.producer}</li> 
-<li type="circle">Año de estreno: ${film.release_date}</li>
-<li type="circle">Rotten Tomatoes: ${film.rt_score}</li>
-</ul>
+<p type="circle">Director: ${film.director}</p>    
+<p type="circle">Productor: ${film.producer}</p> 
+<p type="circle">Año de estreno: ${film.release_date}</p>
+<p type="circle">Rotten Tomatoes: ${film.rt_score}</p>
 </div>
 </div>` 
 });
 
+
+const btnMenu = document.querySelector("#btnMenu");
+const menu = document.querySelector("#menu");
+btnMenu.addEventListener("click",function(){
+    menu.classList.toggle("mostrar");
+});
+
+const subMenuBtn = document.querySelectorAll(".submenu-btn");
+for (let i = 0; i < subMenuBtn.length; i++){
+    subMenuBtn[i].addEventListener("click", function(){
+        if(window.innerWidth < 1024){
+            const subMenu = this.nextElementSibling;
+            const heigh = subMenu.scrollHeight;
+            if (subMenu.classList.contains("desplegar")) {
+                subMenu.classList.remove("desplegar");
+                subMenu.removeAttribute("style");
+
+            }else{
+                subMenu.classList.add("desplegar");
+                subMenu.style.heigh = heigh + "px";
+            }
+        }
+    });    
+}
 /*
 document.getElementById("alphabet").addEventListener("click", (event) => {
     let filmsOrder = moviesSortBy(ghibli.films, alphabet.value);
